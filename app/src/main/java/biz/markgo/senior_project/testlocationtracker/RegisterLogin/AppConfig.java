78ab0aced91c9615987e381d43a39dceac766603
watcharapon <mark_watcharapon@hotmail.com>
2017-02-06 20:10:15 +0700

    Initial commit

---
+++ app/src/main/java/biz/markgo/senior_project/testlocationtracker/RegisterLogin/AppConfig.java
@@ -0,0 +1,8 @@
+package biz.markgo.senior_project.testlocationtracker.RegisterLogin;
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://senior-project.markgo.biz/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://senior-project.markgo.biz/android_login_api/register.php";
+}
